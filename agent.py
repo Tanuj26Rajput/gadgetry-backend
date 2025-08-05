9e607251498556ede8c50976bb7130dd1024fa70
Tanuj Rajput <tanuj26rajput@gmail.com>
2025-08-05 23:02:24 +0530

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,361 @@
+from typing import TypedDict, Literal, List, Annotated, NotRequired
+from pydantic import Field
+from langgraph.graph import StateGraph, START, END
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+import re
+import requests
+import json
+from dotenv import load_dotenv
+from transformers import pipeline
+import os
+import uuid
+from pymongo import MongoClient
+
+load_dotenv()
+
+sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3-Coder-480B-A35B-Instruct",
+    task="text-generation",
+)
+model = ChatHuggingFace(llm=llm)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["gadgetry"]
+session_collection = db["session_data"]
+
+class agentstate(TypedDict):
+    query: str
+    budget: int
+    category: str
+    product: str
+    product_list: List[dict]
+    recommendation: str
+
+prompt_extract = PromptTemplate(
+    template='''
+        You are helpful AI which will process the query given by the user and extract:
+        - budget (in digits only), 0 if got mentioned.
+        - product category (like laptop, mobile, etc.)
+        - use case (like gaming, editing). If no use case is mentioned, take it as "GENERAL".
+
+        Give the output strictly in this format:
+        {{
+            "budget": "...",
+            "category": "...",
+            "usecase": "..."
+        }}
+
+        Query: {query}
+    ''',
+    input_variables=["query"]
+)
+
+prompt_recommend = PromptTemplate(
+    template='''
+        You are a smart electronic gadget assistant.
+
+        You are given:
+        - A list of electronic products (with price, rating and review sentiment)
+        - A user's budget
+        - The product category(e.g., laptop, mobile)
+        - The user's intended use case (e.g., gaming, video editing, general use)
+
+        Your job is to:
+        1. **Analyze all products** in the list and compare them based on:
+            - Use-case scalability
+            - Review sentiment (more positive, fewer negative)
+            - Value for money (within budget, better specs for price)
+            - User ratings
+        2. **Select ONE best products** strictly from the list that best fits the user's needs.
+
+        3. Justify your choice by:
+            - Highlighting strengths of the chosen product
+            - Mentioning why it stands out over others
+            - Referencing review sentiment (e.g., "85% positive reviews") if available
+
+        4. Finish with a friendly and confident final recommendation:
+            - Mention the product name clearly
+            - Include a short summary of why it's the best pick
+
+        STRICT INSTRUCTIONS:
+        - Recommend only from the list.
+        - DO NOT make up products.
+        - Be concise, objective and clear.      
+
+        Budget: {budget}
+        Category: {category}
+        Product Type: {product}
+        Product List:
+        {product_list}
+    ''',
+    input_variables=['budget', 'category', 'product', 'product_list']
+)
+
+prompt_classifier = PromptTemplate(
+    template='''
+        Classify the user query as one of the following:
+        - "recommendation" -> if the user is asking for a product suggestion.
+        - "informational" -> if the user is asking a general question or doubt.
+
+        Query: {query}
+
+        Respond ONLY with one word: recommendation or informational
+    ''',
+    input_variables=["query"]
+)
+
+prompt_followup = PromptTemplate(
+    template='''
+        Is the following query a:
+        - "new" request
+        - "followup" to a previous product recommendation?
+
+        Query: {query}
+
+        Respond ONLY with new or followup
+    ''',
+    input_variables=["query"]
+)
+
+def extract_asin(url: str) -> str:
+    match = re.search(r'/dp/([A-Z0-9]{10})', url)
+    return match.group(1) if match else ""
+
+def fetch_reviews(asin: str) -> List[str]:
+    url = "https://real-time-amazon-data.p.rapidapi.com/product-reviews"
+    headers = {
+        "x-rapidapi-key": os.getenv("RAPIDAPI_KEY"),
+        "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com"
+    }
+    params = {
+        "asin": asin,
+        "country": "IN",
+        "page": 1,
+        "sort_by": "TOP_REVIEWS",
+        "star_rating": "ALL",
+        "verified_purchases_only": "false",
+        "images_or_videos_only": "false",
+        "current_format_only": "false"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        reviews = [r.get("review_text", "") for r in data.get("data", {}).get("reviews", [])]
+        return reviews
+    except Exception as e:
+        print("Review fetch error: ", e)
+        return []
+    
+def analyze_sentiment_bulk(reviews: List[str], batch_size: int = 5) -> dict:
+    if not reviews:
+        return {"positive": 0, "negative": 0, "neutral": 0, "total": 0}
+    
+    sentiment_counts = {"positive": 0, "negative": 0, "neutral": 0}        
+    try:
+        results = sentiment_pipeline(reviews)
+        for r in results:
+            label = r["label"].lower()
+            if label in sentiment_counts:
+                sentiment_counts[label]+=1
+    except Exception as e:
+        print("Local sentiment error: ", e)
+
+    sentiment_counts['total'] = sum(sentiment_counts.values())
+    return sentiment_counts
+
+def route_query(state: agentstate) -> str:
+    query = state["query"]
+    response = model.invoke(prompt_classifier.format(query=query))
+    return response.content.strip().lower()
+
+def classify_query_node(state: agentstate) -> agentstate:
+    return state
+
+def detect_followup_node(state: agentstate) -> agentstate:
+    return state
+
+def handle_informational(state: agentstate) -> agentstate:
+    response = model.invoke(f"User asked: {state['query']}\nAnswer in a helpful and technical but simple way.")
+    state['recommendation'] = response.content.strip()
+    return state
+
+def detect_followup(state: agentstate) -> str:
+    response = model.invoke(prompt_followup.format(query=state['query']))
+    return response.content.strip().lower()
+
+def for_extracting(state: agentstate) -> agentstate:
+    prompt_for_extracting = prompt_extract.format(query=state['query'])
+    response = model.invoke(prompt_for_extracting)
+    try:
+        parsed = json.loads(response.content)
+    except json.JSONDecodeError:
+        print("❌ JSON Parsing failed. Response:", response.content)
+        parsed = {"budget": 0, "category": "unknown", "usecase": "GENERAL"}
+    state["budget"] = int(parsed.get("budget", 0))
+    state["product"] = parsed.get("category", "unknown")
+    state["category"] = parsed.get("usecase", "GENERAL")
+    return state
+
+def product(state: agentstate) -> agentstate:
+    url = "https://real-time-amazon-data.p.rapidapi.com/search"
+    headers = {
+        "x-rapidapi-key": os.getenv("RAPIDAPI_KEY"),
+        "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com"
+    }
+
+    params = {
+        "query": f"{state['category']} {state['product']}",
+        "page": "1",
+        "country": "IN", 
+        "sort_by": "RELEVANCE",
+        "max_price": state['budget'], 
+        "product_condition": "ALL", 
+        "is_prime": "false",
+        "deals_and_discounts": "NONE"
+    }
+    
+    filtered_products = []
+    
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+        results = data.get("data", {}).get("products", [])
+
+        print(f"\n🔍 DEBUG: Found {len(results)} raw products")
+
+        for p in results:
+            product_url = p.get("product_url")
+            asin = extract_asin(product_url)
+            if not asin:
+                continue
+
+            reviews = fetch_reviews(asin)
+            sentiment = analyze_sentiment_bulk(reviews)
+
+            product_info = {
+                "title": p.get("product_title"),
+                "price": p.get("product_minimum_offer_price"),
+                "original_price": p.get("product_original_price", "N/A"),
+                "url": p.get("product_url"),
+                "rating": p.get("product_star_rating", "N/A"),
+                "image": p.get("product_photo", "N/A"),
+                "asin": asin,
+                "review_sentiment": sentiment
+            }
+
+            print("🆕 Product:", product_info["title"])
+            filtered_products.append(product_info)
+
+    except Exception as e:
+        print("API ERROR", e)
+    
+    state["product_list"] = filtered_products
+    return state
+
+def recommendation(state: agentstate) -> agentstate:
+    if not state['product_list']:
+        state["recommendation"] = (
+            "⚠️ Sorry, I couldn't find any relevant products under your budget. "
+            "Please try rephrasing your query or try again later."
+        )
+        return state
+    
+    product_list_str = "\n".join([
+        f"{p['title']} | {p['price']} | {p['original_price']} | {p['rating']} | {p['review_sentiment']['positive']}👍 | {p['review_sentiment']['negative']}👎 | {p['url']}"
+        for p in state['product_list']
+    ])
+
+    prompt_text = prompt_recommend.format(
+        budget=state['budget'],
+        category=state['category'],
+        product=state['product'],
+        product_list=product_list_str
+    )
+
+    response = model.invoke(prompt_text)
+    output = response.content.strip()
+
+    state['recommendation'] = output
+    return state
+
+def handle_followup(state: agentstate) -> agentstate:
+    product_list_str = "\n".join([
+        f"{p['title']} | {p['price']} | {p['rating']}"
+        for p in state['product_list']
+    ]) if isinstance(state['product_list'], list) else "No product Found"
+
+    prompt = f'''
+            You are a helpful AI assistant continuing a product recommendation conversation.
+            Below is your previous recommendation and the list of the products you analyzed:
+
+            Previous Recommendation:
+            {state["recommendation"]}
+
+            Product list:
+            {product_list_str}
+
+            The user is now asking a follow-up question:
+            {state["query"]}
+
+            Only use the products from the list above in your answer.
+            - Do not introduce new products.
+            - You may compare or explain choices from the list.
+            - Be clear, concise and helpful.
+
+            Respond appropriately:
+        '''
+    response = model.invoke(prompt)
+    state['recommendation'] = response.content.strip()
+    return state
+
+graph = StateGraph(agentstate)
+
+graph.add_node("classify_query_node", classify_query_node)
+graph.add_node("handle_informational", handle_informational)
+graph.add_node("detect_followup_node", detect_followup_node)
+graph.add_node("for_extracting", for_extracting)
+graph.add_node("product", product)
+graph.add_node("recommendation", recommendation)
+graph.add_node("handle_followup", handle_followup)
+
+graph.add_edge(START, "classify_query_node")
+graph.add_conditional_edges("classify_query_node", route_query, {
+    "informational": "handle_informational",
+    "recommendation": "detect_followup_node"
+})
+graph.add_conditional_edges("detect_followup_node", detect_followup, {
+    "new": "for_extracting",
+    "followup": "handle_followup"
+})
+graph.add_edge("for_extracting", "product")
+graph.add_edge("product", "recommendation")
+graph.add_edge("recommendation", END)
+graph.add_edge("handle_followup", END)
+graph.add_edge("handle_informational", END)
+
+workflow = graph.compile()
+
+# state: agentstate = {
+#     "query": "",
+#     "budget": 0,
+#     "category": "",
+#     "product": "",
+#     "product_list": [],
+#     "recommendation": ""
+# }
+
+# print("Welcome to Smart Gadget Assistant! Ask anything (type 'exit' to quit)")
+
+# while True:
+#     user_input = input("\n🧑 You: ")
+#     if user_input.lower() in ["exit", "quit"]:
+#         print("Goodbye!")
+#         break
+
+#     state['query'] = user_input
+#     result = workflow.invoke(state)
+#     print("\n🤖 Assistant:", result["recommendation"])
---
+++ main.py
@@ -0,0 +1,298 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, Header
+from fastapi.responses import JSONResponse, RedirectResponse
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import List, Optional
+from pymongo import MongoClient
+from bson import ObjectId
+from datetime import datetime, timezone, timedelta
+from agent import workflow, agentstate
+import traceback
+import bcrypt
+from jose import JWTError, jwt
+import os
+import requests
+from uuid import uuid4
+
+load_dotenv()
+
+#JWT config
+JWT_SECRET=os.getenv("SECRET_KEY")
+JWT_ALGORITHM=os.getenv("JWT_ALGORITHM")
+JWT_EXPIRY_SECONDS=3600
+
+#MongoDB setup
+mongo_client = MongoClient("mongodb://localhost:27017/")
+db = mongo_client["gadgetry"]
+session_collection = db["sessions"]
+user_collection = db['users']
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class QueryRequest(BaseModel):
+    query: str
+    session_id: Optional[str] = "default"
+
+class QueryResponse(BaseModel):
+    recommendation: str
+    product_list: List[dict]
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class PasswordChangeRequest(BaseModel):
+    current_password: str
+    new_password: str
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+    
+@app.get("/")
+def root():
+    return {"message": "Smart gadget assistant is running✅"}
+
+@app.post("/gadget-assist")
+def gadget_assist(request: QueryRequest, authorization: Optional[str] = Header(None)):
+    try:
+        query = request.query
+        session_id = request.session_id
+
+        user_email = None
+        if authorization and authorization.startswith("Bearer "):
+            token = authorization.split(" ")[1]
+            payload = decode_token(token)
+            user_email = payload.get("email")
+
+        if not user_email:
+            return JSONResponse(status_code=403, content={"error": "Authentication required"})
+        
+        composite_session_id = f"{user_email}__{session_id}"
+
+        session = session_collection.find_one({"session_id": composite_session_id})
+
+        if not session:
+            session_state: agentstate = {
+                "session_id": composite_session_id,
+                "query": "",
+                "budget": 0,
+                "category": "",
+                "product": "",
+                "product_list": [],
+                "recommendation": "",
+                "user_email": user_email,
+                "createdate": datetime.now(timezone.utc)
+            }
+        else:
+            session.pop("_id", None)
+            session_state: agentstate = session
+            session_state["user_email"] = user_email
+
+        session_state['query'] = query
+        result = workflow.invoke(session_state)
+        
+        result['session_id'] = composite_session_id
+        result["user_email"] = user_email
+        result['createdate'] = datetime.now(timezone.utc)
+        
+        session_collection.update_one(
+            {"session_id": composite_session_id},
+            {"$set": result},
+            upsert=True
+        )
+
+        return{
+            "recommendation": result['recommendation'],
+            "product_list": result["product_list"],
+            "session_id": composite_session_id
+        }
+    
+    except Exception as e:
+        print("🔥 ERROR in /gadget-assist:", traceback.format_exc())
+        return JSONResponse(
+            status_code=500,
+            content={"error": "Internal server error", "details": str(e)},
+        )
+    
+@app.post("/signup")
+def signup(user: UserCreate):
+    if user_collection.find_one({"email": user.email}):
+        raise HTTPException(status_code=400, detail="Email already exist")
+    
+    hashed_pw = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+
+    user_data = {
+        "name": user.name,
+        "email": user.email,
+        "password": hashed_pw,
+        "created_at": datetime.now(timezone.utc)
+    }
+
+    user_collection.insert_one(user_data)
+
+    token_data = {
+        "sub": user.email,
+        "exp": datetime.now(timezone.utc) + timedelta(hours=12)   
+    }
+    access_token = jwt.encode(token_data, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return {
+        "msg": "Signup Successful",
+        "token_type": "bearer",
+        "access_token": access_token
+    }
+
+@app.post("/login")
+def login(user: UserLogin):
+    user_data = user_collection.find_one({"email": user.email})
+
+    if not user_data:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    if not bcrypt.checkpw(user.password.encode('utf-8'), user_data["password"]):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    payload = {
+        "sub": str(user_data["_id"]),
+        "email": user_data["email"],
+        "exp": datetime.now(timezone.utc) + timedelta(seconds=JWT_EXPIRY_SECONDS)
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "user": {
+            "name": user_data["name"],
+            "email": user_data["email"]
+        }
+    }
+
+    
+def get_current_user(authorization: str = Header(...)):
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid auth header")
+    token = authorization.split(" ")[1]
+    payload = decode_token(token)
+    user_email = payload.get("email")
+    user = user_collection.find_one({"email": user_email})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {
+        "name": user["name"],
+        "email": user["email"],
+        "created_at": user["created_at"]
+    }
+
+@app.get("/profile")
+def get_profile(current_user: dict = Depends(get_current_user)):
+    return {"profile": current_user}
+
+@app.post("/change-password")
+def change_password(data: PasswordChangeRequest, current_user: dict = Depends(get_current_user)):
+    user = user_collection.find_one({"email": current_user["email"]})
+
+    if not bcrypt.checkpw(data.current_password.encode('utf-8'), user["password"]):
+        raise HTTPException(status_code=403, detail="Current password is incorrect")
+    
+    new_hashed_pw = bcrypt.hashpw(data.new_password.encode('utf-8'), bcrypt.gensalt())
+
+    user_collection.update_one(
+        {"email": current_user["email"]},
+        {"$set": {"password": new_hashed_pw}}
+    )
+
+    return {"msg": "Password changed successfully"}
+
+@app.get("/login/google")
+def login_with_google():
+    redirect_uri = (
+        "https://accounts.google.com/o/oauth2/v2/auth"
+        f"?client_id={GOOGLE_CLIENT_ID}"
+        f"&redirect_uri={GOOGLE_REDIRECT_URI}"
+        "&response_type=code"
+        "&scope=openid%20email%20profile"
+    )
+    return RedirectResponse(url=redirect_uri)
+
+@app.get("/auth/google/callback")
+def google_callback(request: Request):
+    code = request.query_params.get("code")
+    if not code:
+        raise HTTPException(status_code=400, detail="Missing authorization code")
+
+    token_url = "https://oauth2.googleapis.com/token"
+    token_data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
+        "grant_type": "authorization_code"
+    }
+    token_response = requests.post(token_url, data=token_data)
+    if not token_response.ok:
+        print("Token fetch error:", token_response.text)
+        raise HTTPException(status_code=400, detail="Failed to fetch access token")
+    
+    token_json = token_response.json()
+    access_token = token_json.get("access_token")
+
+    if not access_token:
+        raise HTTPException(status_code=400, detail="Failed to get access token")
+
+    # Get user info
+    userinfo_response = requests.get(
+        "https://www.googleapis.com/oauth2/v1/userinfo",
+        headers={"Authorization": f"Bearer {access_token}"}
+    )
+
+    if not userinfo_response.ok:
+        print("Userinfo fetch error:", userinfo_response.text)
+        raise HTTPException(status_code=400, detail="Failed to fetch user info")
+    
+    userinfo = userinfo_response.json()
+
+    email = userinfo.get("email")
+    name = userinfo.get("name")
+    picture = userinfo.get("picture")
+
+    user = user_collection.find_one({"email": email})
+    if not user:
+        user_collection.insert_one({
+            "name": name,
+            "email": email,
+            "password": None,
+            "created_at": datetime.now(timezone.utc)
+        })
+
+    payload = {
+        "sub": email,
+        "email": email,
+        "exp": datetime.now(timezone.utc) + timedelta(seconds=JWT_EXPIRY_SECONDS)
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    # Redirect or respond with session info
+    return RedirectResponse(f"http://localhost:5500/frontend/index.html?token={token}", status_code=302)
