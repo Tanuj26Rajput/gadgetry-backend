2347cd8ee0d406d8b093370d2d21df0d2ce82c90
Tanuj26Rajput <tanuj26rajput@gmail.com>
2025-08-21 16:51:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -300,3 +300,12 @@ def google_callback(request: Request):
 
 
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host = "0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=False
+    )
+    
