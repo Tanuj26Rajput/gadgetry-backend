67051e6a69d522751224ad0d14034bd40665a275
Tanuj26Rajput <tanuj26rajput@gmail.com>
2025-08-22 02:51:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://findmygadget.shop"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
