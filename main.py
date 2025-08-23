f1c692ce74383eadc39493321000d305e526116c
Tanuj26Rajput <tanuj26rajput@gmail.com>
2025-08-23 17:20:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -453,4 +453,4 @@ def reset_password(data: ResetPassword):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
